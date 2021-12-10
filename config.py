edb18bae8ffbe7a93222b8e5e2df2700b1cc6595
Faruq Sandi <965723+faruqsandi@users.noreply.github.com>
2021-12-11 00:46:32 +0700

    chapter 9

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['far_uq@yahoo.co.id']
+    
+    POSTS_PER_PAGE = 25
