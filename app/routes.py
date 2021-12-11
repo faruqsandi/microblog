31a29a51bbb3cb166a5a0f23d4c2269392109be0
Faruq Sandi <965723+faruqsandi@users.noreply.github.com>
2021-12-12 06:12:01 +0700

    chapter 10

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,9 @@ from app import db
 from datetime import datetime
 
 
+from app.forms import ResetPasswordRequestForm, ResetPasswordForm
+from app.email import send_password_reset_email
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
@@ -152,3 +155,34 @@ def explore():
     prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
     
     return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+    return render_template('reset_password_request.html', title='Reset Passsword', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not User:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        print(user)
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    else:
+        flash(form.errors)
+    return render_template('reset_password.html', form=form)
