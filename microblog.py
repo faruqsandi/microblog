d94d9975240663fc98af51cb566b5ec67e7f7eca
Faruq Sandi <faruqsandi@gmail.com>
2023-08-04 21:44:29 +0700

    chapter up to 21, but without chapters of deploy

--- microblog.py
+++ microblog.py
@@ -1,8 +1,17 @@
-from app import app, db
-from app.models import User, Post
+from app import create_app, db
+from app.models import User, Post, Notification, Message
 from app import cli
+
+app = create_app()
+cli.register(app)
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {
+        "db": db,
+        "User": User,
+        "Post": Post,
+        "Message": Message,
+        "Notification": Notification,
+    }
